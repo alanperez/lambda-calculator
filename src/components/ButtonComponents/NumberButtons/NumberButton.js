1355cad5ea438d41632f4632e26fa81e17dfce31
Alan Perez <alanp9six@gmail.com>
2019-07-09 18:06:21 -0500

    :sparkles: Component Styling
    Using material ui in order to reduce time spent styling.

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
 const NumberButton = props => {
+  const classes = useStyles();
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button>
+      <Button variant="contained" color="primary" className={classes.button}>
       {props.number}
-      </button>
+      </Button>
     </>
   );
 };
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+// color: #247292
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+  
+}));
+
 
 const OperatorButton = (props) => {
+  const classes = useStyles();
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button>{props.operator.char}</button>
+      <Button variant="contained" style={{backgroundColor:'#247292', color: 'white'}} className={classes.button}>{props.operator.char}</Button>
     </>
   );
 };
